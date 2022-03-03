81ca6a42c0ea6ee3de7963dd727453dfed2b8450
GabrielSantos9 <99453623+GabrielSantos9@users.noreply.github.com>
2022-03-03 20:14:24 -0300

    desafio 1 - finalizado!

---
+++ desafios/desafio01/script/mensagens.js
@@ -0,0 +1,11 @@
+function btn1() {
+    window.alert('Você clicou no 1ºbotão!')
+}
+
+function btn2() {
+    window.alert('Você clicou no 2ºbotão!')
+}
+
+function btn3() {
+    window.alert('Você clicou no 3ºbotão!')
+}
