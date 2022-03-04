14d690c0cae0c9045fd63b5cf525976c1d57a206
GabrielSantos9 <99453623+GabrielSantos9@users.noreply.github.com>
2022-03-04 19:30:34 -0300

    desafio 3 - 90% feito

---
+++ desafios/desafio03/script/ad.js
@@ -0,0 +1,8 @@
+function btn1() {
+   var numin =  window.prompt('Digite um número inteiro qualquer')
+   var menos = 1
+    var mais = 1
+   var rmenos = numin - menos
+   var rmais = mais + numin
+   window.alert(`Antes do ${numin}, temos o número ${rmenos}                                                                       Depois de ${numin} temos o número ${rmais}`)
+}
