3e344f553bdf8a2175bad75b8da38818b4c9c727
GabrielSantos9 <99453623+GabrielSantos9@users.noreply.github.com>
2022-03-08 21:00:49 -0300

    aula 13 - ambiente.js

---
+++ exercicios/Aula 13/ambiente.js
@@ -0,0 +1,30 @@
+var contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 10)
+
+
+
+
+
+
+/*
+var contador = 1
+while (contador <= 6) {
+    console.log(`Passo ${contador}`)
+    contador++ // c = c + 1
+}
+*/
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
