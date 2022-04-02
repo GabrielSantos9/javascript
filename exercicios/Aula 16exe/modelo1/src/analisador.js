b9f2e6d95e1babb02d2cb1cb1d3820acddcd53e8
GabrielSantos9 <99453623+GabrielSantos9@users.noreply.github.com>
2022-04-01 21:05:56 -0300

    Aula 16

--- exercicios/Aula 16exe/modelo1/src/analisador.js
+++ exercicios/Aula 16exe/modelo1/src/analisador.js
@@ -1,16 +1,21 @@
+// O id do html será mudado aqui no .JS, por exemplo, "input#fnum"(HTML) Agora vai passar a ser "num".
 let num = document.querySelector('input#fnum')
 let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
-let valores = []
+let valores = [] // Esse vetor vai ser ultilizado para analisar os dados.
 
+/*  Se(IF) o número for MAIOR que 1, e MENOR que 100, é RETURN TRUE.
+    MAS CASO(ELSE) contrário, é RETURN FALSE.*/
 function isNumero(n) {
-    if(Number(n) >= -1 && Number(n) <= 100) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
         return true
     } else {
         return false
     }
 }
 
+/*  Se na lista o Number(n) for DIFERENTE de -1, é RETURN TRUE.
+    CASO contrário, RETURN FALSE.*/ 
 function inLista(n, l) {
     if (l.indexOf(Number(n)) != -1) {
         return true
@@ -18,16 +23,17 @@ function inLista(n, l) {
         return false
     }
 }
-
+ 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
         valores.push(Number(num.value))
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
-        res.innerHTML = ''
+        
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
+        //Caso contratio, window.aler...
     }
     num.value = ''
     num.focus()
@@ -52,8 +58,9 @@ function finalizar() {
         media = soma / tot
         res.innerHTML = ''
         res.innerHTML += `<p>Ao todo, temos <strong>${tot}</strong> números cadastrados.</p>`
-        res.innerHTML = `<p>O maior valor informado foi <strong>${maior}</strong>.</p>`
-        res.innerHTML = `<p>O menor valor informado foi <strong>${menor}</strong>.</p>`
-        res.innerHTML = `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>O maior valor informado foi <strong>${maior}</strong>.</p>`
+        res.innerHTML += `<p>O menor valor informado foi <strong>${menor}</strong>.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos <strong>${soma}</strong>.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é <strong>${media}</strong>.</p>`
     }
 }
