3e40a752ecdbd40bac602cce2ecccf1b0daf097e
GabrielSantos9 <99453623+GabrielSantos9@users.noreply.github.com>
2022-04-12 20:36:02 -0300

    correção geral

---
+++ desafios/desafio03/src/antes-depois.js
@@ -0,0 +1,9 @@
+function as() {
+   var numin =  window.prompt('Digite um número inteiro qualquer')
+   var menos = 1
+   var mais = 1
+   var soma = Number(numin)+Number(menos)
+   var multiplicacao = Number(numin)-Number(mais)
+   window.alert(`Antes de ${numin}, temos o número ${multiplicacao}
+Depois de ${numin} temos o número ${soma}`)
+}
