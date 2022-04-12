570a2e69f8a3920fd1cae82c1e945f512077ed1b
GabrielSantos9 <99453623+GabrielSantos9@users.noreply.github.com>
2022-04-11 23:20:10 -0300

    desafio 8 - concluído!

---
+++ desafios/desafio08/src/desconto.js
@@ -0,0 +1,17 @@
+function desconto() {
+    let res = document.querySelector('div.res')
+
+    let produto = window.prompt('Qual é o produto que você está comprando?')
+    let preco = window.prompt(`Qual é o preço do ${produto}?`)
+
+    var porcentagem = 10
+    var porcentagemtotal = 100
+    var calculo = Number(porcentagem)*Number(preco)/Number(porcentagemtotal)
+    var subtracao = Number(preco)-Number(calculo)
+
+    res.innerHTML = ''
+    res.innerHTML += `<p>Calculando desconto de <strong>10%</strong> para <strong>${produto}</strong>.</p>`
+    res.innerHTML += `<p>O preço original era <strong>R$ ${preco}</strong>.</p>`
+    res.innerHTML += `<p>Você acaba de ganhar <strong>R$ ${calculo}</strong> de desconto <strong>(-10%)</strong>.</p>`
+    res.innerHTML += `<p>No fim, você vai pagar <strong>R$ ${subtracao}</strong> no produto <strong>${produto}</strong>.</p>`
+}
