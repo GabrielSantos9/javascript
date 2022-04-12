479e2c49ea744796c9e5cfd9817b4e1e9eb8020d
GabrielSantos9 <99453623+GabrielSantos9@users.noreply.github.com>
2022-04-11 21:48:30 -0300

    desafio 7 - concluído!

--- desafios/desafio07/src/conversaomoeda.js
+++ desafios/desafio07/src/conversaomoeda.js
@@ -1,9 +1,11 @@
-function converter() {
-    var cotacaodolar = window.prompt('Antes de mais nada, Quanto está a cotação do dólar agora?')
-    var dinheironacarteira = window.prompt('Quantos R$ você deseja converter?')
 
-    Number(dinheironacarteira)
-    Number (conversaodolar)
-    var conversaodolar = parseFloat(dinheironacarteira)*parseFloat(cotacaodolar)
-    var res = window.alert(`Você converteu R$${dinheironacarteira} em ${conversaodolar}`)
+function converter() {
+    var res = document.querySelector('div.res')
+    var dinheironacarteira = window.prompt('Quantos R$ você deseja converter?')
+    var dolar = 4.70
+
+    var conversaodolar = parseFloat(dinheironacarteira)/parseFloat(dolar)
+    
+    res.innerHTML = ''
+    res.innerHTML += `<p>Você converteu <strong>R$${dinheironacarteira}</strong> em <strong>$${conversaodolar.toFixed(2)}</strong>  !</p>`
 }
