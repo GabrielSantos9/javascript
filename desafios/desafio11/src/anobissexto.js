b4e24955d0c24b43ff53b85eac2e9af580fc2dc0
GabrielSantos9 <99453623+GabrielSantos9@users.noreply.github.com>
2022-04-13 19:34:06 -0300

    Desafio 11 - Concluído!

--- desafios/desafio11/src/anobissexto.js
+++ desafios/desafio11/src/anobissexto.js
@@ -1,4 +1,11 @@
 function verificar() {
+    let res = document.querySelector('div.res')
+
     let ano = window.prompt('Qual é o ano que você quer Verificar?')
-    
+    res.innerHTML = ''
+    if (ano % 400 == 0 || (ano % 4 == 0 && ano % 100 != 0)) {
+        res.innerHTML += `<p>O ano ${ano} <mark class="mrk1">É BISSEXTO</mark></br></p>`
+    } else {
+        res.innerHTML += `<p>O ano ${ano} <mark class="mrk2">NÃO É BISSEXTO</mark></br></p>`
+    }
 }
